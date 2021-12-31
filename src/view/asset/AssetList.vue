13bc46aa71d829f5f9b81704565be6de9f2e167a
bakachan <63496788+bakachanbaby@users.noreply.github.com>
2021-12-31 20:48:58 +0700

    update2

--- src/view/asset/AssetList.vue
+++ src/view/asset/AssetList.vue
@@ -5,7 +5,7 @@
       <!-- Content search -->
       <div class="m-content-search">
         <div class="m-content-search-left">
-          <div class="m-row m-l-search">
+          <div class="m-row m-l-search m-width-27">
             <input
               type="text"
               class="m-input m-input-search"
@@ -14,6 +14,18 @@
               placeholder="Tìm kiếm tài sản"
             />
           </div>
+          <select class="m-combobox m-combobox-search m-width-30" name="">
+            <option value="">Loại tài sản</option>
+            <option value="">HTN</option>
+            <option value="">KHO</option>
+            <option value="">PBT</option>
+          </select>
+          <select class="m-combobox m-combobox-search m-width-30" name="">
+            <option value="">Bộ phận sử dụng</option>
+            <option value="">HTN</option>
+            <option value="">KHO</option>
+            <option value="">PBT</option>
+          </select>
         </div>
         <div class="m-content-search-right">
           <div class="m-row">
@@ -34,163 +46,106 @@
       <!-- Content table -->
       <div class="m-content-box">
         <div class="m-row m-content-table-box">
-        <div class="m-grid">
-          <table id="tbdlEmloyeesList" class="m-table">
-            <thead>
-              <tr>
-                <th class="text-align-center" style="width: 25px !important">
-                  <input type="checkbox" />
-                </th>
-                <th class="text-align-center" style="width: 25px !important">
-                  STT
-                </th>
+          <div class="m-grid">
+            <table id="tbdlEmloyeesList" class="m-table">
+              <thead>
+                <tr>
+                  <th class="text-align-center" style="width: 25px !important">
+                    <input type="checkbox" />
+                  </th>
+                  <th class="text-align-center" style="width: 25px !important">
+                    STT
+                  </th>
 
-                <th class="text-align-left" style="width: 125px">Mã tài sản</th>
-                <th class="text-align-left" style="width: 150px">
-                  Tên tài sản
-                </th>
-                <th class="text-align-left" style="width: 125px">
-                  Loại tài sản
-                </th>
-                <th class="text-align-left">Bộ phận sử dụng</th>
-                <th class="text-align-right" style="width: 100px">Số lượng</th>
-                <th class="text-align-center" style="width: 100px">
-                  Nguyên giá
-                </th>
-                <th class="text-align-center" style="width: 150px">
-                  HM/KH lũy kế
-                </th>
-                <th class="text-align-center" style="width: 150px">
-                  Giá trị còn lại
-                </th>
+                  <th class="text-align-left" style="width: 125px">
+                    Mã tài sản
+                  </th>
+                  <th class="text-align-left" style="width: 150px">
+                    Tên tài sản
+                  </th>
+                  <th class="text-align-left" style="width: 125px">
+                    Loại tài sản
+                  </th>
+                  <th class="text-align-left">Bộ phận sử dụng</th>
+                  <th class="text-align-right" style="width: 100px">
+                    Số lượng
+                  </th>
+                  <th class="text-align-center" style="width: 100px">
+                    Nguyên giá
+                  </th>
+                  <th class="text-align-center" style="width: 150px">
+                    HM/KH lũy kế
+                  </th>
+                  <th class="text-align-center" style="width: 150px">
+                    Giá trị còn lại
+                  </th>
 
-                <th class="text-align-center" style="width: 100px">
-                  Chức năng
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(c, i) in customers"
-                :key="c.CustomerId"
-                @dblclick="rowOnDblClick(c.CustomerId)"
-              >
-                <td class="text-align-center"><input type="checkbox" /></td>
-                <td class="text-align-left">{{ i + 1 }}</td>
-                <td class="text-align-left">{{ c.CustomerCode }}</td>
-                <td class="text-align-left">{{ c.FullName }}</td>
-                <td class="text-align-left">{{ c.Email }}</td>
-                <td class="text-align-left">{{ c.GenderName }}</td>
-                <td class="text-align-right">{{ i + 1 }}</td>
-                <!-- <td class="text-align-center">
+                  <th class="text-align-center" style="width: 100px">
+                    Chức năng
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(c, i) in customers"
+                  :key="c.CustomerId"
+                  @dblclick="rowOnDblClick(c.CustomerId)"
+                >
+                  <td class="text-align-center"><input type="checkbox" /></td>
+                  <td class="text-align-left">{{ i + 1 }}</td>
+                  <td class="text-align-left">{{ c.CustomerCode }}</td>
+                  <td class="text-align-left">{{ c.FullName }}</td>
+                  <td class="text-align-left">{{ c.Email }}</td>
+                  <td class="text-align-left">{{ c.FullName }}</td>
+                  <td class="text-align-right">{{ i + 1 }}</td>
+                  <!-- <td class="text-align-center">
                   {{ formatDataComputed }}
                 </td> -->
-                <!-- methods -->
-                <td class="text-align-right">
-                  {{ formatDate(c.DateOfBirth) }}
-                </td>
-                <!-- filters -->
-                <!-- <td class="text-align-center">
+                  <!-- methods -->
+                  <td class="text-align-right">
+                    {{ formatDate(c.DateOfBirth) }}
+                  </td>
+                  <!-- filters -->
+                  <!-- <td class="text-align-center">
                   {{ c.DateOfBirth | formatDate(c.DateOfBirth) }}
                 </td> -->
-                <td class="text-align-right">{{ c.PhoneNumber }}</td>
-                <td class="text-align-right">{{ c.Address }}</td>
-                <td class="text-align-right">
-                  <div class="m-content-table-row-setting">
-                    <div class="m-content-btn-change"></div>
-                    <div class="m-content-btn-copy"></div>
-                  </div>
-                </td>
-              </tr>
-              <tr
-                v-for="(c, i) in customers"
-                :key="c.CustomerId"
-                @dblclick="rowOnDblClick(c.CustomerId)"
-              >
-                <td class="text-align-center"><input type="checkbox" /></td>
-                <td class="text-align-left">{{ i + 1 }}</td>
-                <td class="text-align-left">{{ c.CustomerCode }}</td>
-                <td class="text-align-left">{{ c.FullName }}</td>
-                <td class="text-align-left">{{ c.Email }}</td>
-                <td class="text-align-left">{{ c.GenderName }}</td>
-                <td class="text-align-right">{{ i + 1 }}</td>
-                <!-- <td class="text-align-center">
-                  {{ formatDataComputed }}
-                </td> -->
-                <!-- methods -->
-                <td class="text-align-right">
-                  {{ formatDate(c.DateOfBirth) }}
-                </td>
-                <!-- filters -->
-                <!-- <td class="text-align-center">
-                  {{ c.DateOfBirth | formatDate(c.DateOfBirth) }}
-                </td> -->
-                <td class="text-align-right">{{ c.PhoneNumber }}</td>
-                <td class="text-align-right">{{ c.Address }}</td>
-                <td class="text-align-center">
-                  <div class="m-content-table-row-setting">
-                    <div class="m-c-t-r-box">
-                      <div
-                        id="m-c-t-r-icon-btn"
-                        class="m-content-table-row-setting-text"
-                      >
-                        Sửa
-                      </div>
-                      <div
-                        id="m-c-t-r-icon-down"
-                        class="m-content-table-row-icon"
-                      ></div>
+                  <td class="text-align-right">{{ c.PhoneNumber }}</td>
+                  <td class="text-align-right">{{ c.Address }}</td>
+                  <td class="text-align-right">
+                    <div class="m-content-table-row-setting">
+                      <div class="m-content-btn-change"></div>
+                      <div class="m-content-btn-copy"></div>
                     </div>
-                    <div class="m-c-t-r-data">
-                      <div class="m-c-t-r-item" value="1">Nhân bản</div>
-                      <div
-                        id=""
-                        class="m-c-t-r-item m-c-t-r-activate"
-                        value="2"
-                      >
-                        Xóa
-                      </div>
-                      <div class="m-c-t-r-item" value="3">Ngừng sử dụng</div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-            
-          </table>
-        </div>
-        
-      </div>
-      <div class="m-paging">
-            <div class="m-paging-left">
-              <div class="m-paging-record">
-                <p>Tổng số:<strong id="totalRecord"> 8 </strong> bản ghi</p>
-              </div>
-              <select
-                class="m-combobox combobox-count"
-                name=""
-                id="cbxPageSize"
-              >
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="30">30</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
-              </select>
-              <div class="m-paging-hover m-paging-prev"></div>
-              <div class="m-paging-number">
-                <div class="m-paging-hover page-number page-number-active">
-                  1
-                </div>
-                <div class="m-paging-hover page-number">2</div>
-                <div class="m-paging-hover page-number">3</div>
-              </div>
-              <div class="m-paging-hover m-paging-next"></div>
-            </div>
-            <div class="m-paging-right"></div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+        </div>
+        <div class="m-paging">
+          <div class="m-paging-left">
+            <div class="m-paging-record">
+              <p>Tổng số:<strong id="totalRecord"> 8 </strong> bản ghi</p>
+            </div>
+            <select class="m-combobox combobox-count" name="" id="cbxPageSize">
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+            <div class="m-paging-hover m-paging-prev"></div>
+            <div class="m-paging-number">
+              <div class="m-paging-hover page-number page-number-active">1</div>
+              <div class="m-paging-hover page-number">2</div>
+              <div class="m-paging-hover page-number">3</div>
+            </div>
+            <div class="m-paging-hover m-paging-next"></div>
+          </div>
+          <div class="m-paging-right"></div>
+        </div>
       </div>
-      
+
       <!-- Content paging -->
     </div>
     <AssetDetail
